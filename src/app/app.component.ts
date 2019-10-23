3e03fecd65d38a7e6f39dd0a225cf6328ea02aea
Jhaziel Sarzuri <jhazielsr@gmail.com>
2019-10-23 18:48:08 -0400

    primera version ejemplo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cliente';
+  descripcion:string;
+  listaEstudiantes:string[];
+
+  constructor() {
+    this.descripcion = "Taller de aplicaciones en Internet";
+    this.listaEstudiantes = ['Carlos', 'Edson', 'Jhaziel', 'Saul', 'Boris', 'Fausto', 'Alan'];
+    console.log(this.descripcion);
+    console.log(this.listaEstudiantes);
+  }
 }
